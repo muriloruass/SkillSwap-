c3da99aa7a1d50755d72fd434b8ca08477f25ef7
Murilo Ruas Lucena <100888358+muriloruass@users.noreply.github.com>
2026-03-12 17:02:49 -0400

    feat: implement job details ui, routing and api integration

--- skillswap-frontend/src/app/app.routes.ts
+++ skillswap-frontend/src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { JobSearchComponent } from './modules/jobs/job-search/job-search';
+import { JobDetailsComponent } from './modules/jobs/job-details/job-details';
 
 export const routes: Routes = [
   // Redireciona a página inicial (temporário) direto pro seu módulo de pesquisa
   { path: '', component: JobSearchComponent },
+  
+  // Rota para o Passo 2: Os detalhes de uma vaga específica
+  { path: 'jobs/:id', component: JobDetailsComponent },
+  
   // Se digitar algo errado na URL, também cai no seu módulo (temporário)
   { path: '**', redirectTo: '' }
 ];
